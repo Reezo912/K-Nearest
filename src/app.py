3b35609fdc0c1cb771f72e83c78fecf6347fc512
Reezo912 <clizaga@gmail.com>
2025-04-14 23:17:14 +0200

    retoques y cosas chulas

--- src/app.py
+++ src/app.py
@@ -7,15 +7,17 @@ from explore import df, X_vectorized
 model = pickle.load(open("../models/Nearest_neighbour.pkl", "rb"))
 
 st.title('Bienvenido al recomendador de peliculas!')
-st.markdown('Este recomendador de peliculas utiliza un algoritmo de Nearest Neighbours')
-st.subheader('Introduce el nombre de la pelicula en Ingles')
+st.page_link('https://github.com/Reezo912', label='Mi Github')
 
-# st.text_input('Aqui escribes tu pelicula', placeholder='Avatar')
+
+st.markdown('Este recomendador de peliculas utiliza un algoritmo de Nearest Neighbours, agrupando automaticamente las peliculas que tienen cosas en comun.')
+
+#st.subheader('Introduce el nombre de la película en inglés')
 
 
 lista_peliculas = df['title']
 
-pelicula_seleccionada = st.selectbox("Nombre de tu pelicula",(lista_peliculas),)
+pelicula_seleccionada = st.selectbox("Nombre de tu pelicula en inglés:",(lista_peliculas), index=None, placeholder='Escribe aqui...')
 
 def get_movie_recommendations(movie_title, dataset, model, x):
             # Obtener el índice de la película
@@ -30,13 +32,27 @@ def get_movie_recommendations(movie_title, dataset, model, x):
             return similar_movies[1:]
 
 
-if st.button("Predict"):
+if st.button("Prediccion!"):
     # Suponiendo que 'df' es tu DataFrame original y 'X_vectorized' la matriz TF-IDF
     recommendations = get_movie_recommendations(pelicula_seleccionada, df, model, X_vectorized)
 
     if recommendations:
-        st.write(f"**Film recommendations for:** '{pelicula_seleccionada}'")
+        st.write(f"**Las recomendaciones para** '{pelicula_seleccionada}' son:")
         for movie, distance in recommendations:
             st.write(f"- {movie}")
+            
+        st.write('Si te han gustado las peliculas, por favor pon nota para que sepa el comportamiento del modelo. Gracias!')
+        sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
+        selected = st.feedback("thumbs")
+        if selected is not None:
+            st.markdown(f"You selected: {sentiment_mapping[selected]}")
+
+
     else:
-        st.write("I didn't find that title! Check spelling!")
+        st.write("No he encontrado ninguna pelicula con ese titulo, revisa que este todo bien escrito!")
+
+
+
+
+
+
